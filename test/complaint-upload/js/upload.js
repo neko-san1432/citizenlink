325d603bfb4cfdee2a72badf72e809c471873904
Krupot <pyrrhuselcidgo@gmail.com>
2025-06-09 02:32:06 +0800

    working on the upload, especially in the duplication part

--- test/complaint-upload/js/upload.js
+++ test/complaint-upload/js/upload.js
@@ -5,29 +5,153 @@ const subCategory = document.getElementById("complaint-subcategory");
 const urgencyLevel = document.getElementById("complaint-urgency");
 const suggestedUnitDispatch = document.getElementById("suggested-unit");
 const location = document.getElementById("complaint-location");
-const decription = document.getElementById("complaint-description");
+const description = document.getElementById("complaint-description");
 //tentative
 const photo = document.getElementById("complaint-photo");
 
 //buttons
-const submit_form = document.getElementById("submit");
+const form = document.getElementById("complaint-form");
 const cancel = document.getElementById("cancel-btn");
+const latitude = 0;
+const longitude = 0;
+const bannedWords = [
+  "yawa",
+  "puta",
+  "putang-ina",
+  "putangina",
+  "tangina",
+  "waa",
+  "fuck",
+  "fck",
+  "shit",
+  "sht",
+];
 
-const latitude = 0
-const longitude = 0
-submit_form.addEventListener("click", async () => {
+form.addEventListener("submit", async () => {
   formChecker();
-  submitForm();
 });
 cancel.addEventListener("click", () => {
+  if (
+    title.value !== "" ||
+    description.value !== "" ||
+    (type.value !== "" && subCategory.value !== "") ||
+    urgencyLevel.value !== "" ||
+    suggestedUnitDispatch.value !== "" ||
+    location.value !== "" ||
+    photo.value !== ""
+  ) {
+    window.addEventListener("beforeunload", function (e) {
+      e.preventDefault(); // Some browsers require this
+    });
+  }
   //going to the base
-});
+  title.value = "";
+  description.value = "";
+  type.value = "";
+  subCategory.value = "";
+  urgencyLevel.value = "";
+  suggestedUnitDispatch.value = "";
+  location.value = "";
+  photo.value = "";
 
-async function formChecker() {}
-async function submitForm() {}
+  //GOTO
+});
+photo.addEventListener("change", () => {});
+
+async function formChecker() {
+  const tester = new RegExp(`\\b(${bannedWords.join("|")})\\b`, "i");
+  const titleCheck = tester.test(title.value);
+  const descriptionCheck = tester.test(description.value);
+  if (titleCheck || descriptionCheck) {
+    alert("Banned words detected");
+    return;
+  }
+  if (
+    title.value === "" ||
+    description.value === "" ||
+    (type.value === "" && subCategory.value === "") ||
+    urgencyLevel.value === "" ||
+    suggestedUnitDispatch.value === "" ||
+    location.value === ""
+  ) {
+    alert("Empty required fields detected");
+    return;
+  }
+  //duplication criteria: location proximity, time window check
+  if (
+    locationProximity() &&
+    timeWindowChecking() &&
+    titleAndDescriptionSimilarities()
+  ) {
+    alert("Duplicate complaint");
+    return;
+  }
+  //if passed all
+  submitForm();
+}
+
+async function locationProximity() {
+  const point1 = turf.point([longitude, latitude]);
+  const { data: dataPoints, error } = await supabase
+    .from("")
+    .select("complaintID,coordinates");
+  if (error) {
+    //tentative lang
+    return false;
+  }
+  // dataPoints.coordinates is a "{}" datatype that "long" and "lat" are the keys
+  for (const coord in dataPoints.coordinates) {
+    const point2 = turf.point([coord.long, coord.lat]);
+    const distance = turf.distance(point1, point2, { units: "kilometers" });
+    if (distance < 0.05) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function timeWindowChecking() {
+    //comparing time
+  return false;
+}
+
+function titleAndDescriptionSimilarities() {
+    //likeness scoring
+  return false;
+}
+
+async function submitForm() {
+  const { data, error: formSubmissionError } = await supabase
+    .from("complaints")
+    .insert([
+      {
+        title: title.value,
+        urgency: urgencyLevel.value,
+        type: type.value,
+        subCategory: subCategory.value,
+        location: location.value,
+        suggestedUnitDispatch: suggestedUnitDispatch.value,
+        user: getUser(),
+      },
+    ]);
+  if (formSubmissionError) {
+    alert("Error uploading complaint: ", formSubmissionError);
+  }
+  const files = Array.from(input.files);
+  if (files.length != 0) {
+    for (const file of files) {
+      const { error: photoUploadError } = await supabase.storage
+        .from("evidence")
+        .upload(`${data.complainID}/${file.name}`, file);
+      if (photoUploadError) {
+        alert("Error upload media: ", photoUploadError);
+      }
+    }
+  }
+}
 function getLocation() {
   if (!navigator.geolocation) {
-    //error message showing the browser doesn't support geolocation
+    alert("Unsupported browser");
     return;
   }
   navigator.geolocation.getCurrentPosition((position) => {
@@ -35,3 +159,12 @@ function getLocation() {
     longitude = position.coords.longitude;
   });
 }
+async function getUser() {
+  const { data, error } = await supabase.auth.getUser();
+  if (error) {
+    console.log("Unauthorized user");
+    return;
+  }
+  return data.id;
+}
+getLocation();
